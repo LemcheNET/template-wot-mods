0618d2c83be4e1e9af2724ec50b709a1db9cf94b
valdemar@lemche.net <valdemar@lemche.net@c61ff787-aa38-a18e-5c64-9df719621b19>
2014-07-31 20:39:37 +0000

    INITIAL

---
+++ generate-file-list.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -f delme.txt ]; then
+	rm -f delme.txt
+fi
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for folder in {atterdag-wot-mods,res_mods,res}; do
+	for d in $($USERPROFILE/bin/find.exe $folder -type d); do
+		echo "    RMDir \"\$INSTDIR\\$d\"" >> delme.txt
+	done
+	for f in $($USERPROFILE/bin/find.exe $folder -type f); do
+		echo "    Delete \"\$INSTDIR\\$f\"" >> delme.txt
+	done
+done
+IFS=$SAVEIFS
+
+tac delme.txt | sed 's/\//\\/g' > file_and_directory_list.txt
+
+rm -f delme.txt
