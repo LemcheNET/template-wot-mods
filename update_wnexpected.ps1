fadfa55cb16d4dce3449e25b0a1ef38d5d86ee54
Valdemar Lemche <valdemar@lemche.net>
2016-03-21 08:41:53 +0100

    Don't need to fetch expected damage for MultiHit log

--- update_wnexpected.ps1
+++ update_wnexpected.ps1
@@ -1,5 +1,5 @@
 $expected_tank_values_version = "26"
-$world_of_tanks_version = "0.9.13"
+$world_of_tanks_version = "0.9.14"
 
 $extensions = @("csv","json")
 foreach($extension in $extensions){
@@ -7,4 +7,4 @@ foreach($extension in $extensions){
 }
 
 Move-Item -Force -Path expected_tank_values.json -Destination res_mods\$world_of_tanks_version\scripts\client\mods\stat\expected_tank_values.json
-Move-Item -Force -Path expected_tank_values.csv -Destination res_mods\$world_of_tanks_version\scripts\client\mods\MultiHitLog\TankData.csv
+#Move-Item -Force -Path expected_tank_values.csv -Destination res_mods\$world_of_tanks_version\scripts\client\mods\MultiHitLog\TankData.csv
