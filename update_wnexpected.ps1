f12a56e8fd4be17fcea0980d2aae4478b0639ed0
Valdemar Lemche <valdemar@lemche.net>
2016-10-11 23:28:02 +0200

    Release 0.9.16.2

--- update_wnexpected.ps1
+++ update_wnexpected.ps1
@@ -1,10 +1,9 @@
-$expected_tank_values_version = "26"
-$world_of_tanks_version = "0.9.15"
+$expected_tank_values_version = "27"
 
 $extensions = @("csv","json")
 foreach($extension in $extensions){
 	Invoke-WebRequest -Uri http://www.wnefficiency.net/exp/expected_tank_values_$expected_tank_values_version.$extension -OutFile expected_tank_values.$extension
 }
 
-Move-Item -Force -Path expected_tank_values.json -Destination res_mods\$world_of_tanks_version\scripts\client\mods\stat\expected_tank_values.json
+Move-Item -Force -Path expected_tank_values.json -Destination res_mods\configs\wotstat\expected_tank_values.json
 #Move-Item -Force -Path expected_tank_values.csv -Destination res_mods\$world_of_tanks_version\scripts\client\mods\MultiHitLog\TankData.csv
