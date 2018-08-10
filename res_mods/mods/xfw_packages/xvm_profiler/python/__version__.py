164f43674e7ecdc9593cfda5517d265e5e7d82f4
Valdemar Lemche <valdemar@lemche.net>
2018-08-10 21:28:04 +0200

    Updated to XVM 1.0.2.4

--- res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.6.9'
-__wot_version__ = '1.0.2.3'
-__revision__ = '8518'
-__branch__ = 'default'
-__node__ = '76133ff00187bc5bc3334fa4995c531b25361fd5'
+__xvm_version__ = '7.6.11'
+__wot_version__ = '1.0.2.4'
+__revision__ = '8554'
+__branch__ = 'wot-1.0.2'
+__node__ = 'f0b1bfcf57cfb35e2f713f3da8724279b95bb3fe'
